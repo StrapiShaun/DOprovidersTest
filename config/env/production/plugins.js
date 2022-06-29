19095f8aeeb674dbaf68891d68d749323d87d52d
Shaun Brown <97027841+StrapiShaun@users.noreply.github.com>
2022-06-29 07:09:00 +0200

    comment out sendmail from production plugins.js

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -13,13 +13,13 @@ module.exports = ({env}) => ({
         }
       },
     }, 
-    email: {
-      config: {
-        provider: 'sendmail',
-        settings: {
-          defaultFrom: 'myemail@protonmail.com',
-          defaultReplyTo: 'myemail@protonmail.com',
-        },
-      },
-    },
+//     email: {
+//       config: {
+//         provider: 'sendmail',
+//         settings: {
+//           defaultFrom: 'myemail@protonmail.com',
+//           defaultReplyTo: 'myemail@protonmail.com',
+//         },
+//       },
+//     },
   })
