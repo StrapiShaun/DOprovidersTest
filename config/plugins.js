35683420d08f5a0f44c46339828e9a586d7c51a4
Shaun Brown <shaun.brown@strapi.io>
2022-07-13 18:34:01 +0200

    version update

--- config/plugins.js
+++ config/plugins.js
@@ -5,19 +5,17 @@ const sendmail = require('sendmail')({
 
 module.exports = ({env}) => ({
     // ...
-    // upload: {
-    //   config: {
-    //     provider: "strapi-provider-upload-do", 
-    //     providerOptions: {
-    //       key: env('DO_SPACE_ACCESS_KEY'),
-    //       secret: env('DO_SPACE_SECRET_KEY'),
-    //       endpoint: env('DO_SPACE_ENDPOINT'),
-    //       space: env('DO_SPACE_BUCKET'),
-    //       directory: env('DO_SPACE_DIRECTORY'),
-    //       cdn: env('DO_SPACE_CDN'),
-    //     }
-    //   },
-    // }, 
+    upload: {
+      config: {
+        breakpoints: {
+          xlarge: 1920,
+          large: 1000,
+          medium: 750,
+          small: 500,
+          xsmall: 64
+        },
+      },
+    },
     email: {
       config: {
         provider: 'sendmail',
